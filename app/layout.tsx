a50a636941f2588f0a6004716b5c9d0d07780f9d
Ahmad <zehanxtech@gmail.com>
2026-01-22 17:22:21 +0500

    gfhj

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 import type { Metadata } from "next";
 export const metadata: Metadata = {
-	title: "Zehanx Technologies | B2B Software Solutions, AI/ML, Cybersecurity & Enterprise Systems",
+	title: "zehanx Technologies | B2B Software Solutions, AI/ML, Cybersecurity & Enterprise Systems",
 	description: "Zehanx Technologies - Empowering businesses with comprehensive B2B software solutions in AI/ML, cybersecurity, and enterprise systems.",
 	icons: {
 		icon: "/unnamed.png",
